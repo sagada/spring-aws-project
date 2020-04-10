baccc9e2c7b38696f294c684d2311c4c9f5e3730
sagada <wlatmdqja@naver.com>
2020-04-11 01:06:36 +0900

    컨트롤러, 테스트 코드, 엔티티, 레포지토리 설정 완료

---
+++ src/main/java/com/spring/aws/service/PostsService.java
@@ -0,0 +1,38 @@
+package com.spring.aws.service;
+
+
+import com.spring.aws.domain.posts.Posts;
+import com.spring.aws.domain.posts.PostsRepository;
+import com.spring.aws.web.dto.PostsResponseDto;
+import com.spring.aws.web.dto.PostsSaveRequestDto;
+import com.spring.aws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto)
+    {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto)
+    {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id)
+    {
+        Posts entity = postsRepository.findById(id).orElseThrow( ()-> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id ));
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/spring/aws/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.spring.aws.web;
+
+import com.spring.aws.service.PostsService;
+import com.spring.aws.web.dto.PostsResponseDto;
+import com.spring.aws.web.dto.PostsSaveRequestDto;
+import com.spring.aws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto)
+    {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto)
+    {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+}
