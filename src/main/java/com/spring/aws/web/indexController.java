333e40e97577f7d1d78eae0a167c918241b30eab
sagada <wlatmdqja@naver.com>
2020-04-13 23:28:43 +0900

    깃 머스테치 적용후 테스트 완료

---
+++ src/main/java/com/spring/aws/web/indexController.java
@@ -0,0 +1,13 @@
+package com.spring.aws.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
